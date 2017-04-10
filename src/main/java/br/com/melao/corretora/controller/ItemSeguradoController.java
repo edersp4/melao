f7415e05ae5d7a2ec83c00a14fa4ef4313104278
Ederson da Silva <edsilvasp@hotmail.com>
2017-04-10 20:33:31 -0300

    ajuste layout

--- src/main/java/br/com/melao/corretora/controller/ItemSeguradoController.java
+++ src/main/java/br/com/melao/corretora/controller/ItemSeguradoController.java
@@ -2,7 +2,6 @@ package br.com.melao.corretora.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.melao.corretora.model.ItemSeguroCarro;
@@ -13,10 +12,11 @@ public class ItemSeguradoController {
 	@RequestMapping(value="/item/cadastro-seguroCarro")
 	public ModelAndView cadastro(ItemSeguroCarro itemSeguroCarro ) {
 		ModelAndView view = new ModelAndView("item/cadastro-seguroCarro");
+		view.addObject(itemSeguroCarro);
 		return view;
 	}
 
-	@RequestMapping(value="/item/gravar" , method=RequestMethod.POST)
+	@RequestMapping(value="/item/gravar")
 	public ModelAndView gravar(ItemSeguroCarro itemSeguroCarro) {
 		ModelAndView view = new ModelAndView("item/cadastro-seguroCarro");
 		return view;
